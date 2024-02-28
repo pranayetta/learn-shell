f1560fb0287a4a062902db3cb55bd9d57a660de5
pranay <pranayetta.1@gmail.com>
2024-02-27 22:28:26 -0700

    First Shell Script

--- printing.sh
+++ printing.sh
@@ -1,1 +1,11 @@
 echo hello world
+#I like to print the text in different colours
+
+# echo -e "\e[COLmMessage\e[0m"
+
+echo -e "\e[31mPranay is a good boy\e[0m"
+echo -e "\e[32mPranay is a good boy\e[0m"
+echo -e "\e[33mPranay is a good boy\e[0m"
+echo -e "\e[34mPranay is a good boy\e[0m"
+echo -e "\e[35mPranay is a good boy\e[0m"
+echo -e "\e[36mPranay is a good boy\e[0m"
